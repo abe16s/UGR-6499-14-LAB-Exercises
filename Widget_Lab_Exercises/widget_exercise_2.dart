46f5ae6cd55263d0bc6485920575e4001f2beba4
Abenezer Seifu <abenezerseifu123@gmail.com>
2024-04-02 16:13:32 +0300

    Exercise 2

---
+++ Widget_Lab_Exercises/widget_exercise_2.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text("Cart"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.shopping_cart, size: 50,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Shopping Cart",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    Text(
+                        "Verify your quality and click checkout",
+                        style: TextStyle(color: Colors.grey),
+                      )
+                  ],
+                )
+              ],
+            ),
+
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 200,
+                        child: Image.asset("assets/car.jpg")),
+                      Column(
+                        children: [
+                          Text("Salads"),
+                          Text("\$15.00"),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Icon(Icons.add_circle),
+                          Text("1.0"),
+                          Icon(Icons.minimize_rounded),
+                  
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 200,
+                        child: Image.asset("assets/car.jpg")),
+                      Column(
+                        children: [
+                          Text("Angel Hair"),
+                          Text("\$22.9"),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Icon(Icons.add_circle),
+                          Text("1.0"),
+                          Icon(Icons.minimize_rounded),
+                  
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+
+      )
+    );
+  }
+}
